6c9d0a91a1e7c8392b43ebb62b91619d2cbdea85
best019 <danrachel220@gmail.com>
2022-05-19 15:06:33 +0000

    generates the death details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  {
+    path: 'death-details',
+    loadChildren: () => import('./pages/death-details/death-details.module').then( m => m.DeathDetailsPageModule)
+  }
 ];
 
 @NgModule({
